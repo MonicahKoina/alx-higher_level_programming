6bf269d537b1a5fac624d856966da4929e7fd135
MonicahKoina <monicahkoina1@gmail.com>
2024-04-08 09:17:02 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const count = process.argv.length;
-
-if (count === 2) {
-	console.log('No argument');
-} else if (count === 3) {
-	console.log('Argument found');
+if (process.argv.length <= 2) {
+  console.log('No argument');
 } else {
-	console.log('Arguments found');
-}
+  if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
