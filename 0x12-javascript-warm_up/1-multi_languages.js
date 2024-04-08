5835463e1a052592dc8bf8a87ce7c29d54ae1859
MonicahKoina <monicahkoina1@gmail.com>
2024-04-08 07:21:55 -0700

    task 0

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const Clang='C is fun';
-const Pylang='Python is cool';
-const JSlang='JavaScript is amazing';
+const cFun = 'C is fun';
+const pyCool = 'Python is cool';
+const jAmaze = 'JavaScript is amazing';
 
-console.log(Clang);
-console.log(Pylang);
-console.log(JSlang);
+console.log(cFun);
+console.log(pyCool);
+console.log(jAmaze);
