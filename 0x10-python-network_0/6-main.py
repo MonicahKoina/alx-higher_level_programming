4b65a67f70baacfc310305e9d4ea7ee1f1abbcb3
MonicahKoina <monicahkoina1@gmail.com>
2024-04-29 11:19:48 -0700

    correction

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-""" Test function find_peak """
-find_peak = __import__('6-peak').find_peak
+"""Defines a peak-finding algorithm."""
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1])
+
+def find_peak(list_of_integers):
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
+
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
