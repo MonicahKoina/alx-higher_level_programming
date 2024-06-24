605fa1c632868d50bd8265d99715d4c6dc893f00
MonicahKoina <monicahkoina1@gmail.com>
2024-06-24 01:34:16 -0700

    remove character

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-# Author - bamidele Adefolaju
-
-
 def remove_char_at(str, n):
-    if n < 0
+    strcopy = ""
+    count = 0
+    for i in range(len(str)):
+        if count == n:
+            strcopy += ''
+        elif count != n:
+            strcopy += str[count]
+        count += 1
+    return strcopy
